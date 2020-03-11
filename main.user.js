5bce98833c7f770e9992fa07cdd167daf92c278d
flaribbit <1149761294@qq.com>
2020-03-11 15:23:23 +0800

    add: 知乎和专栏

--- main.user.js
+++ main.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name         一键复制wiki公式
+// @name         一键复制公式
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Click to copy equation in Wikipedia
 // @author       flaribbit
 // @match        http://*.wikipedia.org/*
 // @match        https://*.wikipedia.org/*
+// @match        https://www.zhihu.com/question/*
+// @match        https://zhuanlan.zhihu.com/p/*
 // @grant        none
 // ==/UserScript==
 
@@ -14,14 +16,30 @@
     // Your code here...
     window.tempbox=document.createElement("input");
     document.body.appendChild(window.tempbox);
-    let copyTex=function(){
-        window.tempbox.value='$'+this.alt+'$';
-        window.tempbox.select();
-        document.execCommand('copy');
-    }
-    let eqs=document.getElementsByClassName("mwe-math-fallback-image-inline");
-    for(let i=0;i<eqs.length;i++){
-        eqs[i].onclick=copyTex;
-        eqs[i].title='点击即可复制公式';
+    let host=document.location.host;
+    if(host.search('wikipedia')>0){
+        let copyTex=function(){
+            window.tempbox.value='$'+this.alt+'$';
+            window.tempbox.select();
+            document.execCommand('copy');
+        }
+        let eqs=document.getElementsByClassName("mwe-math-fallback-image-inline");
+        for(let i=0;i<eqs.length;i++){
+            eqs[i].onclick=copyTex;
+            eqs[i].title='点击即可复制公式';
+        }
+    }else if(host.search('zhihu')>0){
+        let copyTex=function(){
+            window.tempbox.value='$'+this.getAttribute("data-formula")+'$';
+            window.tempbox.select();
+            document.execCommand('copy');
+        }
+        let imgs=document.getElementsByTagName("img");
+        for(let i=0;i<imgs.length;i++){
+            if(imgs[i].alt){
+                imgs[i].onclick=copyTex;
+                imgs[i].title='点击即可复制公式';
+            }
+        }
     }
 })();
