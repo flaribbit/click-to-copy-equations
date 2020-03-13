45c4472e30bcb882ebdf00a493caa152d9525e93
flaribbit <1149761294@qq.com>
2020-03-13 14:14:05 +0800

    修复知乎新加载的回答无法复制的问题 添加复制闪烁提示

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         一键复制公式
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Click to copy equation in Wikipedia
 // @author       flaribbit
 // @match        http://*.wikipedia.org/*
@@ -13,19 +13,26 @@
 
 (function() {
     'use strict';
-    // Your code here...
     window.tempbox=document.createElement("input");
     document.body.appendChild(window.tempbox);
     let host=document.location.host;
+    let el=document.createElement("style");
+    el.innerText="@keyframes aniclick{0%{background:#03A9F400}20%{background:#03A9F47F}100%{background:#03A9F400}}";
+    document.head.appendChild(el);
+    let clearAnimation=function(){
+        this.style.animation="";
+    }
     if(host.search('wikipedia')>=0){
         let copyTex=function(){
             window.tempbox.value='$'+this.alt+'$';
             window.tempbox.select();
             document.execCommand('copy');
+            this.style.animation="aniclick .4s";
         }
         let eqs=document.getElementsByClassName("mwe-math-fallback-image-inline");
         for(let i=0;i<eqs.length;i++){
             eqs[i].onclick=copyTex;
+            eqs[i].addEventListener("animationend",clearAnimation);
             eqs[i].title='点击即可复制公式';
         }
     }else if(host.search('zhihu')>=0){
@@ -33,11 +40,22 @@
             window.tempbox.value='$'+this.getAttribute("data-formula")+'$';
             window.tempbox.select();
             document.execCommand('copy');
+            this.style.animation="aniclick .4s";
         }
-        let eqs=document.querySelectorAll("img[alt]");
-        for(let i=0;i<eqs.length;i++){
-            eqs[i].onclick=copyTex;
-            eqs[i].title='点击即可复制公式';
+        let ref=function(){
+            if(document.visibilityState=="visible"){
+                let eqs=document.querySelectorAll("img[alt]");
+                for(let i=0;i<eqs.length;i++){
+                    eqs[i].onclick=copyTex;
+                    eqs[i].addEventListener("animationend",clearAnimation);
+                    eqs[i].title='点击即可复制公式';
+                }
+            }
+        }
+        if(document.location.href.search('question')>=0){
+            setInterval(ref,1000);
+        }else{
+            ref();
         }
     }
 })();
